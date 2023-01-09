c3ed75d47a8f61884222d04507a43f7601053da1
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-09 18:19:09 +0330

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -50,13 +50,13 @@ st.write(
 c1, c2, c3 = st.columns(3)
 with c1:
     st.info('**Analyst: [Emanoel](https://twitter.com/Astiran91)**', icon="📌")
-    c1.image(Image.open('Images/analyst2.JPG'))
+    #c1.image(Image.open('Images/analyst2.JPG'))
 with c2:
     st.info('**Database: [Flipside Crypto](https://flipsidecrypto.xyz/)**', icon="📚")
-    c2.image(Image.open('Images/flipside.JPG'))
+    #c2.image(Image.open('Images/flipside.JPG'))
 with c3:
     st.info('**Provided for: [MetricsDao](https://metricsdao.xyz/)**', icon="💡")
-    c3.image(Image.open('Images/metricsdao.JPG'))
+    #c3.image(Image.open('Images/metricsdao.JPG'))
 
 
 
