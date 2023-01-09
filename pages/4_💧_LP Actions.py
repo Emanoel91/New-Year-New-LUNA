f6c91cf360796fefeea777113af1167eb99a7eff
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-10 00:09:17 +0330

    Update 4_💧_LP Actions.py

--- pages/4_💧_LP Actions.py
+++ pages/4_💧_LP Actions.py
@@ -38,43 +38,46 @@ with open('style.css')as f:
 # flipside API
 @st.cache(ttl=600)
 def get_data(query1):
-    if query1 == 'Contracts':
-              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/4f470ff4-e192-4be3-94cf-dd143ca5f358/data/latest')
-    elif query1 == 'Total Contracts':
-              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/e6f8b14f-ca91-42aa-9bd6-be3c56e5c33c/data/latest')
-    elif query1 == 'Statistic Contracts':
-              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/744b7f5c-f0e5-41f4-ba4d-ad25a7e295a7/data/latest')
+    if query1 == 'Liquidity pools':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/cac60939-7270-4b4c-ada5-c47dbb3350ec/data/latest')
+    elif query1 == 'Liquidity pools statistic Average':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/8f667e4d-f334-4e6a-9426-1b7dfccf08c2/data/latest')
+    elif query1 == 'Liquidity pools statistic Median':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/ead7df54-8d50-414b-bc27-f75e51900f3e/data/latest')
+    elif query1 == 'Total Transactions Count of each Action':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/236f33de-dcc6-4dc1-875f-4dde90250398/data/latest')
+    elif query1 == 'User Type':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/5bf044f7-a81c-4aee-a565-57e9231956a0/data/latest')
+    elif query1 == 'Average Daily Liquidity Providers':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/ae7f585b-58cd-40e9-aaca-e5d116fde30e/data/latest')
+    elif query1 == 'Median Daily Liquidity Providers':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/2b17d842-e3a2-4c2e-ac75-6656d9fd836b/data/latest')
+    elif query1 == 'Number of New Liquidity Providers':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/7f7a1fe1-4eac-4a03-9fda-bc0f5e029f1b/data/latest')
+    elif query1 == 'Total New Liquidity Providers':
+              return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/7ad5a1fd-f041-4e39-b5d2-9a1a994070ae/data/latest')
     return None
 
-Contracts = get_data('Contracts')
-Total_Contracts = get_data('Total Contracts')
-Statistic_Contracts = get_data('Statistic Contracts')
+Liquidity_pools = get_data('Liquidity pools')
+Liquidity_pools_statistic_Average = get_data('Liquidity pools statistic Average')
+Liquidity_pools_statistic_Median = get_data('Liquidity pools statistic Median')
+Total_Transactions_Count_of_each_Action = get_data('Total Transactions Count of each Action')
+User_Type = get_data('User Type')
+Average_Daily_Liquidity_Providers = get_data('Average Daily Liquidity Providers')
+Median_Daily_Liquidity_Providers = get_data('Median Daily Liquidity Providers')
+Number_of_New_Liquidity_Providers = get_data('Number of New Liquidity Providers')
+Total_New_Liquidity_Providers = get_data('Total New Liquidity Providers')
 
-df = Total_Contracts
+df = Total_New_Liquidity_Providers
 c1, c2 = st.columns(2)
     
 with c1:
-        st.metric(label='Total Number of New Contracts (2023)', value=df['Total Contracts'])
+        st.metric(label='Total Number of New Liquidity Providers (2023)', value=df['New Liquidity Providers'])
 
 
-df = Contracts
-fig = sp.make_subplots(specs=[[{'secondary_y': True}]])
-fig.add_trace(go.Bar(x=df['Date'], y=df['Contracts'], name='New Contracts'), secondary_y=False)
-fig.add_trace(go.Line(x=df['Date'], y=df['Cummulative Contracts'], name='Cummulative New Contracts'), secondary_y=True)
-fig.update_layout(title_text='Number of New Contracts per Day')
-fig.update_yaxes(title_text='', secondary_y=False)
-fig.update_yaxes(title_text='', secondary_y=True)
+df = Liquidity_pools
+fig = px.bar(df, x='Date', y='Transactions Count', color='Action', title='Number of Actions per Day', log_y=False)
+fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
 
-df = Statistic_Contracts
-c1, c2 = st.columns(2)
-with c1:
-        fig = px.bar(df, x='Year', y='Average', title='🟡 Daily Average Number of New Contracts')
-        fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='')
-        st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-               
-with c2:
-        fig = px.bar(df, x='Year', y='Median', title='🟡 Daily Median Number of New Contracts')
-        fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='')
-        st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
