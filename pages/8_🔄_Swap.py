66ca978e53c8fc095554ce2a7820ddf217ad933a
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-11 20:26:41 +0330

    Update 8_🔄_Swap.py

--- pages/8_🔄_Swap.py
+++ pages/8_🔄_Swap.py
@@ -97,7 +97,7 @@ for i in df['Action'].unique():
         name=i,
         x=df.query("Action == @i")['Date'],
         y=df.query("Action == @i")['Volume'],
-        mode='Bars',
+        mode='Bar',
         stackgroup='one',
         groupnorm='percent'
      ))
