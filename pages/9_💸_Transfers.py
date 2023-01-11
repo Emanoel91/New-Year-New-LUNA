1fc1472639d1b11cdecff5861f75ebe9ad1c8ce4
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-12 02:00:53 +0330

    Update 9_💸_Transfers.py

--- pages/9_💸_Transfers.py
+++ pages/9_💸_Transfers.py
@@ -60,5 +60,4 @@ with c2:
         st.metric(label='Total Transfers Count(2023)', value=df['Total Transfers Count'])
 with c3:
         st.metric(label='Total Receivers Count', value=df['Total Receivers Count'])
-with c4:
-        st.metric(label='Total Senders Count', value=df['Total Senders Count']) 
+ 
