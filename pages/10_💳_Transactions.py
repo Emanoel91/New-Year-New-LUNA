c042a0d838c15e5aa437833b79dda9ce92aec1e4
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-12 07:16:11 +0330

    Update 10_💳_Transactions.py

--- pages/10_💳_Transactions.py
+++ pages/10_💳_Transactions.py
@@ -90,13 +90,20 @@ with c1:
       fig.update_layout(legend_title='Success', legend_y=0.5)
       fig.update_traces(textinfo='percent+label', textposition='inside')
       st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+      
+      fig = px.bar(df, x='Success', y='TXs Count', color='Success', title='', log_y=False)
+      fig.update_layout(showlegend=False, xaxis_title=None, legend_title='Success', yaxis_title='TXs', xaxis={'categoryorder':'total ascending'})
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 with c2:
       df = Transactions_Status  
       fig = px.pie(df, values='TX Fee', names='Success', title='Total Transaction Fees')
       fig.update_layout(legend_title='Success', legend_y=0.5)
       fig.update_traces(textinfo='percent+label', textposition='inside')
-      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)      
-     
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+      
+      fig = px.bar(df, x='Success', y='TX Fee', color='Success', title='', log_y=False)
+      fig.update_layout(showlegend=False, xaxis_title=None, legend_title='Success', yaxis_title='$LUNA', xaxis={'categoryorder':'total ascending'})
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)     
 
 
 
