ff3831a023b2e35885f7ad44da5d47a7e8ad71bd
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-12 04:21:28 +0330

    Update 1-📱_Addresses.py

--- pages/1-📱_Addresses.py
+++ pages/1-📱_Addresses.py
@@ -7,9 +7,9 @@ import plotly.subplots as sp
 from PIL import Image
 
 theme_plotly = None # None or streamlit
-
+luna = PIL.Image.open('luna-logo.png')
 # Structure
-st.set_page_config(page_title='Addresses - New Year New LUNA', page_icon=':chart_with_upwards_trend:', layout='wide')
+st.set_page_config(page_title='Addresses - New Year New LUNA', page_icon=luna, layout='wide')
 st.title('📱 Addresses')
 
 # Cover
